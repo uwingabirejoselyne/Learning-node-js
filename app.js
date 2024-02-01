f4c09eb13f723bcf03f51669622980555688d912
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-01 12:13:33 +0200

    routing request

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    const url = req.url
+    if(url ==='/'){
+        res.write('<html>')
+        res.write('<head><title>Routing request</title></head>')
+        res.write('<body><form method="Post"><input type="text" name="message"></body>')
+        res.write('</html>')
+        return res.end()
+    }
 })
 server.listen(5000)
