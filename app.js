7da15c8146709660891e3ef2a2e08523e3b72f42
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-02 15:33:55 +0200

    redirecting request

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const http = require('http')
+const fs = require('fs')
 const server = http.createServer((req,res)=>{
     const url = req.url
+    const method = req.method
     if(url ==='/'){
         res.write('<html>')
         res.write('<head><title>Routing request</title></head>')
-        res.write('<body><form method="Post"><input type="text" name="message"></body>')
+        res.write('<body><form method="Post"><input type="text" name="message"><button type="submit">send</button></form></button></body>')
         res.write('</html>')
+        return res.end()
+    }
+    if(url ==='/message' && method ==='POST'){
+        fs.writeFileSync('message.txt', 'Hello, world!', 'utf-8')
+        res.statusCode = 302
+        res.setHeader('Location','/')
         return res.end()
     }
 })
