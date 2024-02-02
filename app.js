5acf1602ef3a289952bf6ea6739ff69445c69131
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-02 16:43:49 +0200

    parsing body request

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req,res)=>{
-    const url = req.url
-    const method = req.method
-    if(url ==='/'){
-        res.write('<html>')
-        res.write('<head><title>Routing request</title></head>')
-        res.write('<body><form method="Post"><input type="text" name="message"><button type="submit">send</button></form></button></body>')
-        res.write('</html>')
-        return res.end()
-    }
-    if(url ==='/message' && method ==='POST'){
-        fs.writeFileSync('message.txt', 'Hello, world!', 'utf-8')
-        res.statusCode = 302
-        res.setHeader('Location','/')
-        return res.end()
-    }
-})
-server.listen(5000)
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Routing request</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    res.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+    fs.writeFileSync("message.txt", "Hello, world!", "utf-8");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+});
+server.listen(5000, () => {
+  console.log("hello");
+});
