ae852fe6063666ead005aaf93c8293de6cb77d00
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-07 07:50:27 +0200

    Parsing an incoming request

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
+// Parse URL-encoded form data
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Route for /add-product
 app.use("/add-product", (req, res, next) => {
-  console.log("this is the middleware");
-  res.send("<h1> Add Product</h1>");
+  res.send("<form action ='/product' method='POST'> <input type='text' name='title'><button type='submit'>Send</button></form>");
 });
+
+// Route for /product
+app.use('/product', urlencodedParser, (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+// Root path
 app.use("/", (req, res, next) => {
-  console.log("this is the middleware");
-  res.send("<h1> Hello from the middleware</h1>");
+  res.send("<h1>Hello from the middleware</h1>");
 });
 
 app.listen(5000);
