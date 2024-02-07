fcb56f54c490a12d09c485e35516bad8b502fb38
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-07 10:42:03 +0200

    adding a 404 error page

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const shopRoutes = require("./routes/shop");
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next)=>{
+res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(5000);
