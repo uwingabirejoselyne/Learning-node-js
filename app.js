f989466e69c206e629dbfe0ddb049d457df95039
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-07 10:50:04 +0200

    filtering paths

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
 res.status(404).send('<h1>Page not found</h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get("/add-product", (req, res, next) => {
-    res.send("<form action ='/product' method='POST'> <input type='text' name='title'><button type='submit'>Send</button></form>");
+    res.send("<form action ='/admin/add-product' method='POST'> <input type='text' name='title'><button type='submit'>Send</button></form>");
   });
   
   // Route for /product
- router.post('/product',urlencodedParser ,(req, res, next) => {
+ router.post('/add-product',urlencodedParser ,(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
   });
