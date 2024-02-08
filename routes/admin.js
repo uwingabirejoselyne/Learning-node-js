268abd3fd836f21787ff4045cbe8cf3c0d55842b
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-08 20:06:17 +0200

    created add-product page

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,18 @@
-const path = require('path')
-const express = require('express');
-const router = express.Router()
+const path = require("path");
+const express = require("express");
+const router = express.Router();
 const bodyParser = require("body-parser");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
-  });
-router.use(express.static(path.join(__dirname,'public')))
-  
-  // Route for /product
- router.post('/add-product',urlencodedParser ,(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-  });
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
 
-module. exports = router
+// Route for /product
+router.post("/add-product", urlencodedParser, (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
