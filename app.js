2207e266c540413ee7b4d7d3da56392aff3ecede
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-02-08 20:18:07 +0200

    created shop page

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use('/admin',adminRoutes);
-app.use(shopRoutes);
+
+app.use('/',shopRoutes);
 app.use(express.static(path.join(__dirname,'public')))
 app.use((req,res,next)=>{
 res.status(404).send('<h1>Page not found</h1>')
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const shopRouter = express.Router();
-shopRouter.get("/", (req, res, next) => {
-  res.send("<h1>Hello from the middleware</h1>");
-});
+const path = require('path')
+// shopRouter.get("/", (req, res, next) => {
+//   res.send("<h1>Hello from the middleware</h1>");
+// });
+shopRouter.get("/",(req,res,next)=>{
+  res.sendFile(path.join(__dirname,'../','views','shop.html'))
+})
 module.exports = shopRouter;
